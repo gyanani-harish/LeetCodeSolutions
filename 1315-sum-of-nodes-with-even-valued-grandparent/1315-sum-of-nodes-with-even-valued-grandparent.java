e7abd5ff4f551026fa7031edfc66418cce0ca93c
Harish Gyanani <gyanani.harish@gmail.com>
2022-05-07 17:51:25 +0530

    Time: 13 ms (6.37%), Space: 54 MB (7.10%) - LeetHub

---
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.java
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    static class TreeNodeWithParent {
+        TreeNodeWithParent parent;
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNodeWithParent(TreeNode node, TreeNodeWithParent parent){
+            this.val = node.val;
+            this.left = node.left;
+            this.right = node.right;
+            this.parent = parent;
+        }
+        TreeNodeWithParent(TreeNode node){
+            this.val = node.val;
+            this.left = node.left;
+            this.right = node.right;
+            parent = null;
+        }
+    }
+    public int sumEvenGrandparent(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int total = 0;
+        Queue<TreeNodeWithParent> queue = new LinkedList<>();
+        queue.add(new TreeNodeWithParent(root));
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i=0;i<size;i++){
+                TreeNodeWithParent node = queue.poll();
+                if(node.parent!=null && node.parent.parent!=null && node.parent.parent.val%2==0){
+                    total+=node.val;
+                }
+                if(node.left!=null){
+                    queue.add(new TreeNodeWithParent(node.left, node));
+                }
+                if(node.right!=null){
+                    queue.add(new TreeNodeWithParent(node.right, node));
+                }
+            }
+        }
+        return total;
+                //    6,
+                //   7,8,
+                // 2,7,1,3,
+        //9,null,1,4,null,null,null,5
+    }
+}
