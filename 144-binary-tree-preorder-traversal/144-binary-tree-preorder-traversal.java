932669990c71a7c136835a1dac3d96889ba2b05e
Harish Gyanani <gyanani.harish@gmail.com>
2022-07-16 23:30:26 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (30.69%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
@@ -14,7 +14,8 @@
  * }
  */
 class Solution {
-    public List<Integer> preorderTraversal(TreeNode root) {
+    public List<Integer> preorderTraversal2(TreeNode root) {
+        //with recursion
         List<Integer> answerList = new ArrayList<>();
         if(root == null){ 
             return new ArrayList<>();
@@ -29,5 +30,24 @@ class Solution {
             answerList.addAll(rightList);
         }
         return answerList;
+    }
+    public List<Integer> preorderTraversal(TreeNode root) {
+        //with recursion but save addAll call
+        return preorderTraversalSaveAddAll(root, new ArrayList<Integer>());
+    }
+    public List<Integer> preorderTraversalSaveAddAll(TreeNode root, List<Integer> ansList){
+        if(root == null){
+            return ansList;
+        }
+        ansList.add(root.val);
+        preorderTraversalSaveAddAll(root.left, ansList);
+        preorderTraversalSaveAddAll(root.right, ansList);
+        return ansList;
+    }
+    
+    
+    public List<Integer> preorderTraversal3(TreeNode root) {
+        //with for loop
+        return null;
     }
 }
