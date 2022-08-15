71d845b87e455f5394e2cd53133dcdee23dd8540
Harish Gyanani <gyanani.harish@gmail.com>
2022-08-15 17:05:13 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (72.83%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -9,7 +9,25 @@
  * }
  */
 class Solution {
+    //using prev pointer without O(n) spacce
     public ListNode reverseList(ListNode head) {
+        if(head == null || head.next==null){
+            return head;
+        }
+        ListNode prev = null;
+        ListNode current = head;//1
+        ListNode nextNode = head.next;//2
+        while(nextNode!=null){
+            current.next = prev;//1->null, 2->1, 3->2, 4->3
+            prev = current;//1, 2, 3, 4
+            current = nextNode;//2, 3, 4, 5
+            nextNode = current.next;//3, 4, 5, null
+        }
+        current.next = prev;
+        return current;
+    }
+    //space O(n), time O(n)
+    public ListNode reverseList2(ListNode head) {
         if(head == null || head.next==null){
             return head;
         }
