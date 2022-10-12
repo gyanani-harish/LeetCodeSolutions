61b0fd36952d693935b2b9ce42adadc1c1a0bf9f
Harish Gyanani <gyanani.harish@gmail.com>
2022-10-12 20:38:53 +0530

    Time: 30 ms (5.42%), Space: 51.2 MB (5.01%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,46 +1,56 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        if(nums.length == 0){
-            return new int[]{-1,-1};
-        }
-        return targetRange(nums, target);
-    }
-    private int[] targetRange(int[] nums, int target){
-        int index = binarySearch(nums, target);
-        if(index == -1){
-            return new int[]{-1,-1};
-        }
-        int l= index - 1;
-        int r = index + 1;
-        while(l>-1 || r<nums.length){
-            boolean found = false;
-            if(l>=0 && nums[l]==target){
-                found = true;
-                l--;
+    
+    /**
+    * Example - 5,7,7,8,8,10 - 
+    * mid = 0+5/2 = 2, found 7
+    * 7<target, go higher side, 
+    * now low = 3, high =5, mid = 3+5/2=4
+    * 8 is found
+    * recurse - 0, 3 for more 8's
+    * mid = 0+3/2 = 1
+    * 7<8 -> low = 2, high = 3, mid = 2+3/2 = 2
+    * 7<8 -> low = 3, high = 3, mid = 3+3/2 = 3
+    * 8 == 8
+    *
+    * Example - 1,2,3,8,8,*8*,8,8,8,10,12
+    * 
+    */
+     public int[] searchRange(int[] nums, int target) {
+            if (nums.length == 0) {
+                return new int[]{-1, -1};
             }
-            if(r<nums.length && nums[r]==target){
-                found = true;
-                r++;
+            Set<Integer> result = binarySearch(nums, target, 0, nums.length - 1);
+            Iterator<Integer> iterator = result.iterator();
+            int l = -1;
+            int r = -1;
+
+            while (iterator.hasNext()) {
+                if (l == -1) {
+                    l = iterator.next();
+                }
+                r = iterator.hasNext() ? iterator.next() : l;
             }
-            if(found == false){
-                break;
-            }
+            return new int[]{l, r};
         }
-        return new int[]{l+1, r-1};
-    }
-    private int binarySearch(int[] nums, int target){
-        int low = 0;
-        int high = nums.length-1;
-        while(low<=high){
-            int midIndex = (low+high)/2;
-            if(nums[midIndex] == target){
-                return midIndex;
-            } else if(nums[midIndex]< target){
-                low = midIndex + 1;
-            } else {
-                high = midIndex - 1;
+    private Set<Integer> binarySearch(int[] nums, int target, int low, int high) {
+            Set<Integer> resultSet = new TreeSet<>();
+            while (low <= high) {
+                int midIndex = (low + high) / 2;
+                if (nums[midIndex] == target) {
+                    resultSet.add(midIndex);
+                    if (low <= midIndex - 1 && nums[midIndex - 1] == target && (!resultSet.contains(midIndex-1) || !resultSet.contains(low))) {
+                        resultSet.addAll(binarySearch(nums, target, low, midIndex - 1));
+                    }
+                    if (midIndex + 1 <= high && nums[midIndex + 1] == target && (!resultSet.contains(midIndex+1) || !resultSet.contains(high))) {
+                        resultSet.addAll(binarySearch(nums, target, midIndex + 1, high));
+                    }
+                    return resultSet;
+                } else if (nums[midIndex] < target) {
+                    low = midIndex + 1;
+                } else {
+                    high = midIndex - 1;
+                }
             }
+            return resultSet;
         }
-        return -1;
-    }
 }
