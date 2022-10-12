ddccdfb7cc986ee351e776e3d0f75a66175ee6ac
Harish Gyanani <gyanani.harish@gmail.com>
2022-10-12 22:26:01 +0530

    Time: 437 ms (14.81%), Space: 44.1 MB (38.72%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.kt
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.kt
@@ -6,51 +6,65 @@ class Solution {
         return binarySearch(nums, target, 0, nums.size - 1)
     }
 
+    private fun setResult(nums: IntArray, target: Int, midIndex: Int, resultSet: IntArray): Boolean {
+        if (midIndex < 0 || midIndex > nums.size - 1) {
+            return false
+        }
+        if(nums[midIndex] != target){
+            return false
+        }
+        var result = false
+        if (midIndex < resultSet[0] || resultSet[0] == -1) {
+            resultSet[0] = midIndex
+            result = true
+        }
+        if (midIndex > resultSet[1]) {
+            resultSet[1] = midIndex
+            result = true
+        }
+        return result
+    }
+    
     private fun binarySearch(nums: IntArray, target: Int, _low: Int, _high: Int): IntArray {
         //println("Window search $_low and $_high")
         var low = _low
         var high = _high
-        val resultSet = IntArray(2,{ -1 })
+        val resultArr = IntArray(2, { -1 })
         while (low <= high) {
             val midIndex = (low + high) / 2
             if (nums[midIndex] == target) {
-                if(midIndex<resultSet[0] || resultSet[0] == -1){
-                    resultSet[0] = midIndex
+                val isAddedMin = setResult(nums, target, midIndex - 1, resultArr)
+                val isAddedMax = setResult(nums, target, midIndex + 1, resultArr)
+                if (!isAddedMin || !isAddedMax) {
+                    setResult(nums, target, midIndex, resultArr)
                 }
-                if(midIndex>resultSet[1]){
-                   resultSet[1] = midIndex
-                }
-                if (low <= midIndex - 1 && nums[midIndex - 1] == target && (!resultSet.contains(midIndex - 1) || !resultSet.contains(
-                        low
-                    ))
-                ) {
-                    val result = binarySearch(nums, target, low, midIndex - 1)
-                    if(result[0]!=-1 && (result[0]<resultSet[0] || resultSet[0] == -1)){
-                        resultSet[0] = result[0]
+                val lowIndex = midIndex - 2
+                val maxIndex = midIndex + 2
+                if (low <= lowIndex && nums[lowIndex] == target) {
+                    val result = binarySearch(nums, target, low, lowIndex)
+                    if (result[0] != -1 && (result[0] < resultArr[0] || resultArr[0] == -1)) {
+                        resultArr[0] = result[0]
                     }
-                    if(result[1]>resultSet[1]){
-                        resultSet[1] = result[1]
+                    if (result[1] > resultArr[1]) {
+                        resultArr[1] = result[1]
                     }
                 }
-                if (midIndex + 1 <= high && nums[midIndex + 1] == target && (!resultSet.contains(midIndex + 1) || !resultSet.contains(
-                        high
-                    ))
-                ) {
-                    val result = binarySearch(nums, target, midIndex + 1, high)
-                    if(result[0]!=-1 && (result[0]<resultSet[0] || resultSet[0] == -1)){
-                        resultSet[0] = result[0]
+                if (maxIndex <= high && nums[maxIndex] == target) {
+                    val result = binarySearch(nums, target, maxIndex, high)
+                    if (result[0] != -1 && (result[0] < resultArr[0] || resultArr[0] == -1)) {
+                        resultArr[0] = result[0]
                     }
-                    if(result[1]>resultSet[1]){
-                        resultSet[1] = result[1]
+                    if (result[1] > resultArr[1]) {
+                        resultArr[1] = result[1]
                     }
                 }
-                return resultSet
+                return resultArr
             } else if (nums[midIndex] < target) {
                 low = midIndex + 1
             } else {
                 high = midIndex - 1
             }
         }
-        return resultSet
+        return resultArr
     }
 }
